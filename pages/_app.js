b47e9188163218fe707f99856abccf245c877a81
Davcode1234 <dpietruszewski12@gmail.com>
2022-11-28 15:55:28 +0100

    Create welcome sec and fix some bugs

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import Layout from "../components/Layout";
 import { GlobalStyle } from "../styles/GlobalStyles";
 import { theme } from "../lib/theme";
 import { ThemeProvider } from "styled-components";
+import "../styles/normalize.css";
 
 function MyApp({ Component, pageProps }) {
   return (
