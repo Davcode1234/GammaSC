0d86004f7c07baf4ee74bc57b26d147691a76fbd
Davcode1234 <dpietruszewski12@gmail.com>
2022-12-28 15:59:58 +0100

    Add images on about page

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,65 @@
 import { Fragment } from "react";
 import SubpagesHeader from "../components/SubpagesHeader/SubpagesHeader";
+import { HeroImg, TeamWrapper } from "../styles/pages/About.styles";
+
+const ImagesURLS = [
+  {
+    img: "/AboutPage/Gamma_Aleksander.webp",
+    id: 1,
+  },
+  {
+    img: "/AboutPage/Gamma_Anna.webp",
+    id: 2,
+  },
+  {
+    img: "/AboutPage/Gamma_Beata.webp",
+    id: 3,
+  },
+  {
+    img: "/AboutPage/Gamma_Damian.webp",
+    id: 4,
+  },
+  {
+    img: "/AboutPage/Gamma_Edyta.webp",
+    id: 5,
+  },
+  {
+    img: "/AboutPage/Gamma-gabriela.webp",
+    id: 6,
+  },
+  {
+    img: "/AboutPage/Gamma-jaromir.webp",
+    id: 7,
+  },
+  {
+    img: "/AboutPage/Gamma-jerzy.webp",
+    id: 8,
+  },
+  {
+    img: "/AboutPage/Gamma-joanna.webp",
+    id: 9,
+  },
+  {
+    img: "/AboutPage/Gamma-kamil.webp",
+    id: 10,
+  },
+  {
+    img: "/AboutPage/Gamma-marta.webp",
+    id: 11,
+  },
+  {
+    img: "/AboutPage/Gamma-pan-rysiu.webp",
+    id: 12,
+  },
+  {
+    img: "/AboutPage/Gamma-sebastian.webp",
+    id: 13,
+  },
+  {
+    img: "/AboutPage/Gamma-weronika.webp",
+    id: 14,
+  },
+];
 
 export default function About() {
   const aboutData = {
@@ -18,6 +78,20 @@ export default function About() {
         headerSmall={aboutData.headerSmall}
         text={aboutData.text}
       ></SubpagesHeader>
+
+      <TeamWrapper>
+        {ImagesURLS.map((hero) => {
+          return (
+            <HeroImg
+              src={hero.img}
+              width={305}
+              height={350}
+              alt={"hero img"}
+              key={hero.id}
+            ></HeroImg>
+          );
+        })}
+      </TeamWrapper>
     </>
   );
 }
---
+++ styles/pages/About.styles.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styled from "styled-components";
+
+export const TeamWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  max-width: 50%;
+`;
+
+export const HeroImg = styled(Image)``;
